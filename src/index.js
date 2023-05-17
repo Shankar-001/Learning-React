67468583b87dd81a0a6cb328bbe8e91b3340a81a
Shankar-001 <praveen01.ugec20@iiitranchi.ac.in>
2023-05-17 22:51:07 +0530

    Second Commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function Greeting() {
-  return <h2>My first Component</h2>
+  return (
+    <React.Fragment>
+      <div>
+        <h3>Hello People</h3>
+        <ul>
+          <li>
+            <a href='#'>Hello world</a>
+          </li>
+        </ul>
+      </div>
+      <h2>Hello Everyone</h2>
+    </React.Fragment>
+  )
 }
+
+// function Greeting() {
+//   return React.createElement('h2', {}, ' Hello world');
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Greeting />)
